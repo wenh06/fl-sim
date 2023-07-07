694b93062bdb228ecc88f5882740b7340ed426ef
WEN Hao <wenh06@gmail.com>
2023-07-07 16:17:21 +0800

    move frequently used test functions to utils.misc

--- fl_sim/utils/misc.py
+++ fl_sim/utils/misc.py
@@ -1,16 +1,19 @@
 """
 """
 
+import collections
 import inspect
 import os
+import platform
 import random
+import subprocess
 import types
 import warnings
 from collections import OrderedDict, defaultdict
 from copy import deepcopy
 from functools import wraps
 from pathlib import Path
-from typing import Any, Callable, Union, Optional, Sequence
+from typing import Any, Callable, Union, Optional, Sequence, Tuple, List
 
 import numpy as np
 import torch
@@ -463,3 +466,69 @@ def add_kwargs(func: callable, **kwargs: Any) -> callable:
         return func(*args, **filtered_kwargs)
 
     return wrapper
+
+
+def execute_cmd(
+    cmd: Union[str, List[str]], raise_error: bool = True
+) -> Tuple[int, List[str]]:
+    """Execute shell command using `Popen`.
+
+    Parameters
+    ----------
+    cmd : str or list of str
+        Shell command to be executed,
+        or a list of .sh files to be executed.
+    raise_error : bool, default True
+        If True, error will be raised when occured.
+
+    Returns
+    -------
+    exitcode : int
+        Exit code returned by `Popen`.
+    output_msg : list of str
+        Outputs from `stdout` of `Popen`.
+
+    """
+    shell_arg, executable_arg = True, None
+    s = subprocess.Popen(
+        cmd,
+        shell=shell_arg,
+        executable=executable_arg,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        close_fds=(not (platform.system().lower() == "windows")),
+    )
+    debug_stdout = collections.deque(maxlen=1000)
+    print("\n" + "*" * 10 + "  execute_cmd starts  " + "*" * 10 + "\n")
+    while 1:
+        line = s.stdout.readline().decode("utf-8", errors="replace")
+        if line.rstrip():
+            debug_stdout.append(line)
+            # print(line)
+        exitcode = s.poll()
+        if exitcode is not None:
+            for line in s.stdout:
+                debug_stdout.append(line.decode("utf-8", errors="replace"))
+            if exitcode is not None and exitcode != 0:
+                error_msg = " ".join(cmd) if not isinstance(cmd, str) else cmd
+                error_msg += "\n"
+                error_msg += "".join(debug_stdout)
+                s.communicate()
+                s.stdout.close()
+                print("\n" + "*" * 10 + "  execute_cmd failed  " + "*" * 10 + "\n")
+                if raise_error:
+                    raise subprocess.CalledProcessError(exitcode, error_msg)
+                else:
+                    output_msg = list(debug_stdout)
+                    return exitcode, output_msg
+            else:
+                break
+    s.communicate()
+    s.stdout.close()
+    output_msg = list(debug_stdout)
+
+    print("\n" + "*" * 10 + "  execute_cmd succeeded  " + "*" * 10 + "\n")
+
+    exitcode = 0
+
+    return exitcode, output_msg
--- test/test_custom.py
+++ test/test_custom.py
@@ -6,76 +6,7 @@ For the test to succeed, it should be run in the root directory of the project w
 **NOT** in the test directory with `pytest -s -v test_custom.py`.
 """
 
-import collections
-import platform
-import subprocess
-from typing import Union, List, Tuple
-
-
-def execute_cmd(
-    cmd: Union[str, List[str]], raise_error: bool = True
-) -> Tuple[int, List[str]]:
-    """Execute shell command using `Popen`.
-
-    Parameters
-    ----------
-    cmd : str or list of str
-        Shell command to be executed,
-        or a list of .sh files to be executed.
-    raise_error : bool, default True
-        If True, error will be raised when occured.
-
-    Returns
-    -------
-    exitcode : int
-        Exit code returned by `Popen`.
-    output_msg : list of str
-        Outputs from `stdout` of `Popen`.
-
-    """
-    shell_arg, executable_arg = False, None
-    s = subprocess.Popen(
-        cmd,
-        shell=shell_arg,
-        executable=executable_arg,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        close_fds=(not (platform.system().lower() == "windows")),
-    )
-    debug_stdout = collections.deque(maxlen=1000)
-    print("\n" + "*" * 10 + "  execute_cmd starts  " + "*" * 10 + "\n")
-    while 1:
-        line = s.stdout.readline().decode("utf-8", errors="replace")
-        if line.rstrip():
-            debug_stdout.append(line)
-            # print(line)
-        exitcode = s.poll()
-        if exitcode is not None:
-            for line in s.stdout:
-                debug_stdout.append(line.decode("utf-8", errors="replace"))
-            if exitcode is not None and exitcode != 0:
-                error_msg = " ".join(cmd) if not isinstance(cmd, str) else cmd
-                error_msg += "\n"
-                error_msg += "".join(debug_stdout)
-                s.communicate()
-                s.stdout.close()
-                print("\n" + "*" * 10 + "  execute_cmd failed  " + "*" * 10 + "\n")
-                if raise_error:
-                    raise subprocess.CalledProcessError(exitcode, error_msg)
-                else:
-                    output_msg = list(debug_stdout)
-                    return exitcode, output_msg
-            else:
-                break
-    s.communicate()
-    s.stdout.close()
-    output_msg = list(debug_stdout)
-
-    print("\n" + "*" * 10 + "  execute_cmd succeeded  " + "*" * 10 + "\n")
-
-    exitcode = 0
-
-    return exitcode, output_msg
+from fl_sim.utils.misc import execute_cmd
 
 
 def test_custom_dataset_algorithm():
