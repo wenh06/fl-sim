66dde5e524434eea2d8f7738d98b758c74b87582
WEN Hao <wenh06@gmail.com>
2025-07-20 22:02:25 +0800

    add utils function compute_clustering_metrics

--- fl_sim/utils/misc.py
+++ fl_sim/utils/misc.py
@@ -21,10 +21,13 @@ from copy import deepcopy
 from functools import wraps
 from numbers import Number
 from pathlib import Path
-from typing import Any, Callable, List, Optional, Sequence, Tuple, Union
+from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple, Union
 
 import numpy as np
 import torch
+from scipy.optimize import linear_sum_assignment
+from sklearn.metrics import adjusted_rand_score, normalized_mutual_info_score
+from sklearn.preprocessing import LabelEncoder
 from torch_ecg.utils import get_kwargs, get_required_args
 
 from .const import LOG_DIR
@@ -42,6 +45,8 @@ __all__ = [
     "find_longest_common_substring",
     "add_kwargs",
     "compute_sparsity",
+    "execute_cmd",
+    "compute_clustering_metrics",
 ]
 
 
@@ -542,3 +547,57 @@ def compute_sparsity(model_or_tensor: Union[torch.nn.Module, torch.Tensor]) -> f
         n_params += param.data.numel()
         nonzeros += param.abs().sign().sum().int().item()
     return nonzeros / n_params
+
+
+def compute_clustering_metrics(y_true: np.ndarray, y_pred: np.ndarray) -> Dict[str, float]:
+    """Compute clustering metrics, including ARI, NMI, and clustering accuracy.
+
+    Parameters
+    ----------
+    y_true : np.ndarray
+        True cluster labels of the data points.
+    y_pred : np.ndarray
+        Predicted cluster labels of the data points.
+
+    Returns
+    -------
+    metrics : Dict[str, float]
+        A dictionary containing the computed clustering metrics.
+
+    """
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    metrics = {
+        "ARI": adjusted_rand_score(y_true, y_pred),
+        "NMI": normalized_mutual_info_score(y_true, y_pred),
+        "ACC": _clustering_accuracy(y_true, y_pred),
+    }
+    metrics = {k: v.item() if isinstance(v, np.generic) else v for k, v in metrics.items()}
+    return metrics
+
+
+def _clustering_accuracy(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """Compute clustering accuracy with optimal label matching using Hungarian algorithm.
+
+    Parameters
+    ----------
+    y_true : np.ndarray
+        True cluster labels of the data points.
+    y_pred : np.ndarray
+        Predicted cluster labels of the data points.
+
+    Returns
+    -------
+    acc : float
+        The clustering accuracy.
+    """
+    label_true = LabelEncoder().fit_transform(y_true)
+    label_pred = LabelEncoder().fit_transform(y_pred)
+
+    D = max(label_true.max(), label_pred.max()) + 1
+    w = np.zeros((D, D), dtype=np.int64)
+    for i in range(label_pred.size):
+        w[label_pred[i], label_true[i]] += 1
+
+    row_ind, col_ind = linear_sum_assignment(w.max() - w)
+    acc = w[row_ind, col_ind].sum() / y_pred.size
+    return acc.item()
